d2c35e25dfbaaba67235740ce0018f4b733396d3
Gabe L <gabe@fijiwebdesign.com>
2024-01-27 17:57:27 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const screenshotRouter = require('./routes/screenshot');
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle CORS preflight requests
+app.options('*', cors());
+
+app.use('/api/screenshot', screenshotRouter);
+
+
+// Start the server
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`Server is running on `, `http://localhost:${PORT}`);
+});
---
+++ routes/screenshot.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { chromium } = require("playwright");
+
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+  try {
+
+    const id = req.params.id
+
+    if (!id) throw new TypeError('id parameter required')
+
+    const browser = await chromium.launch();
+
+    const path = `public/screenshots/memezoo-meme-${id}.png`
+    const host = 'http://localhost:3000'
+ 
+    const page = await browser.newPage();
+    await page.setViewportSize({ width: 1280, height: 1080 });
+    await page.goto(host + "/memes/image?id=" + id);
+    await page.waitForLoadState('networkidle');
+    await page.locator('.meme-image').screenshot({ path });
+    await browser.close();
+    res.json({
+      data: {
+        url: path
+      }
+    })
+  } catch (error) {
+    console.error('Error capturing screenshot:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
