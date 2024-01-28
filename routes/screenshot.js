89aecded1a501dd9029e6a3e74dd594a7c9364d9
Gabe L <gabe@fijiwebdesign.com>
2024-01-28 12:37:35 +0300

    json api

--- routes/screenshot.js
+++ routes/screenshot.js
@@ -4,33 +4,43 @@ const fs = require('fs')
 
 const router = express.Router();
 
-router.get('/:slug', async (req, res) => {
+const PORT = process.env.PORT
+const HOST_URL = 'http://localhost:' + PORT
+
+router.post('/create', async (req, res) => {
   try {
 
-    const slug = req.params.slug
-    const id = slug?.slice(slug?.lastIndexOf('-') + 1).replace('.png', '')
+    const json = req.body
+    const viewportSize = json.viewportSize || { width: 1280, height: 1080 }
+    const url = json.url
+    const locator = json.locator
 
-    if (!id) throw new TypeError('id parameter required')
+    console.log('json', json)
 
     const browser = await chromium.launch();
 
-    const path = `public/screenshot/${slug}`
-    const host = 'https://memezoo.app'
-    const url = host + "/memes/image?id=" + id
+    const imageName = 'test.png'
+    const urlPath = `screenshot/${imageName}`
+    const filePath = `public/${urlPath}`
 
     console.log('url', { url, id, path })
  
     const page = await browser.newPage();
-    await page.setViewportSize({ width: 1280, height: 1080 });
+    await page.setViewportSize(viewportSize);
     await page.goto(url);
     await page.waitForLoadState('networkidle');
-    await page.locator('.meme-image').screenshot({ path });
+    if (locator) {
+      await page.locator(locator).screenshot({ path: filePath });
+    } else {
+      await page.screenshot({ path: filePath });
+    }
     await browser.close();
 
-    const stream = fs.createReadStream(path)
+    const data = {
+      url: `${HOST_URL}/${urlPath}`
+    }
 
-    res.setHeader('Content-Type', 'image/png')
-    stream.pipe(res)
+    res.json(data)
     
   } catch (error) {
     console.error('Error capturing screenshot:', error);
